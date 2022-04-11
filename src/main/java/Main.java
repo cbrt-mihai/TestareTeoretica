b568b087b8e72187a72e2530d62aa2fcefe6964e
cbrt-mihai <cbrt.mihai@gmail.com>
2022-04-11 23:49:02 +0300

    error for negative parameters
    error for negative parameters

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,36 @@
+/*      if ( k < 0 || a > b || s < 0 ) {System.out.println("error"); return null;}
+        List primes = new ArrayList();
+        int copy;
+        int digit;
+        int sum;
+        boolean found;
+        int number, divisor;
+        for(number = a; primes.size() <= k && number <= b; number++)
+        {
+            if(number == 1) continue;
+            found = false;
+            for(divisor = 2; !found && divisor <= sqrt(number); divisor++)
+            {
+                if(number % divisor == 0) found = true;
+            }
+            if(!found)
+            {
+                copy = number;
+                sum = 0;
+                while(copy != 0)
+                {
+                    digit = copy % 10;
+                    copy = copy / 10;
+                    sum += digit;
+                }
+                if(sum == s) primes.add(number);
+            }
+        }
+        return primes;
+ */
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static java.lang.Math.sqrt;
@@ -10,6 +42,8 @@ public class Main {
 
     // Gaseste primele K numere prime, ce au suma cifrelor S, din intervalul [a,b]
     public static List findPrimes(int k, int a, int b, int s) {
+        if ( k < 0 || a > b || s < 0 ) {System.out.println("error"); return null;}
+
         List primes = new ArrayList();
         int copy;
         int digit;
@@ -25,7 +59,6 @@ public class Main {
             {
                 if(number % divisor == 0) found = true;
             }
-
 
             if(!found)
             {
